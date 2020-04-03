6c5998cfbebd8fab576da453470f391bf89573bb
niklas <niklasp.hastbacka@gmail.com>
2020-04-03 14:00:33 +0300

    added better css

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 let inputField = document.getElementById('text-field')
 let wordLengthElement = document.getElementById('wordleng')
+let wordElements = document.getElementById('words')
+inputField.focus()
 
-inputField.addEventListener('keyup', function(){
-    let letters = inputField.value
-    wordLengthElement.textContent = letters.length
+let letters = 0
+let words = 0
+
+wordLengthElement.textContent = letters
+wordElements.textContent = words
+
+inputField.addEventListener('keyup', function () {
+    words = inputField.value.split(/\w+/)
+    characters = inputField.value
+
+    wordLengthElement.textContent = characters.length
+    wordElements.textContent = words.length - 1
 })
